ceb7bc4caaf88bdd53114c81cd7cb5de3f4c6e62
Josafat Contreras <josafat005@outlook.com>
2023-01-18 13:32:09 -0600

    Agregamos lista de canciones

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const songs = [
+    {
+        name: 'song 1',
+        path: 'music/Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'image/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'music/Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'image/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'music/Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'image/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'music/Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'image/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'music/Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'image/cover 5.png'
+    }
+]
